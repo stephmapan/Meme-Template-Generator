9f2687453e61d47dd8ac4bee1c32d2c3b6ae852f
Stephen Ma-Pan <77118596+stephmapan@users.noreply.github.com>
2024-07-17 15:27:34 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from "react"
+import Header from "./Header"
+import Meme from "./Meme"
+
+export default function App() {
+    return (
+        <div className='app'>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
